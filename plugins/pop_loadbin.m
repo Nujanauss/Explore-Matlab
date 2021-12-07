1e210a4370949703bd2e9bf0f78d7975818ff613
unknown <appacclap@hotmail.co.uk>
2021-12-07 17:54:46 +0100

    Small refactor

--- plugins/pop_loadbin.m
+++ plugins/pop_loadbin.m
@@ -1,15 +1,15 @@
 function [EEG, com] = pop_loadbin(filepath, varargin)  
     com = '';
-    if nargin < 1 % No file provided
-        [filename, path] = uigetfile({'*.BIN;' 'All BIN files';}, 'Select a BIN file'); 
-        if filename == 0
-            error('File selection cancelled.')
-        else
-            filepath = strcat(path,filename);
-            loadbin(filepath);
-        end
-    else % file was specified in the call
+    if nargin > 0  % File provided in call
+        loadbin(filepath);
+        return;
+    end
+
+    [filename, path] = uigetfile({'*.BIN;' 'All BIN files';}, 'Select a BIN file'); 
+    if filename == 0
+        error('File selection cancelled.')
+    else
+        filepath = strcat(path, filename);
         loadbin(filepath);
     end
-        
 end
